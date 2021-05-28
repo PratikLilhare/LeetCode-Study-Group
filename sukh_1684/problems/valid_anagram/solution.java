41a1419bb9a04019c0041522a631579b727e255b
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-28 20:35:16 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- sukh_1684/problems/valid_anagram/solution.java
+++ sukh_1684/problems/valid_anagram/solution.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> a = new HashMap<Character,Integer>();
-        for(int i=0;i<s.length();i++){
-            char c= s.charAt(i);
-            a.put(c,a.getOrDefault(c,0)+1);
+        int len1=s.length();
+        int len2= t.length();
+        if(len1!= len2){
+            return false;
         }
-                for(int i=0;i<t.length();i++){
-                   char c= t.charAt(i);
-                    if(!a.containsKey(c)){
-                        return false;
-                    }
-                    else{
-                        a.put(c,a.get(c)-1);
-                    }
-                }
-        for( int i: a.values()){
-            if(i!=0)
+           HashMap<Character,Integer> check = new HashMap<>();
+          for(int i=0;i<len1;i++){
+              char c = s.charAt(i);
+              check.put(c,check.getOrDefault(c,0)+1);
+          }
+        for(int i=0;i<len2;i++){
+            char c = t.charAt(i);
+            if(!check.containsKey(c)){
                 return false;
+            }
+            int temp = check.get(c)-1;
+            if(temp == -1){
+                return false;
+            }
+            check.put(c,temp);
         }
+        
         
         return true;
     }
