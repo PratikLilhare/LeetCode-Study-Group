bbf204eb0071d1375062da220bb558cab6a8e0fd
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-28 19:48:18 +0000

    Sync LeetCode submission - Reverse String (java)

--- sukh_1684/problems/reverse_string/solution.java
+++ sukh_1684/problems/reverse_string/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public void reverseString(char[] s) {
-        int n= s.length;
+        int n = s.length;
         for(int i=0;i<n/2;i++){
             char temp = s[i];
-            s[i]= s[n-1-i];
-            s[n-1-i]=temp;
+            s[i]=s[n-i-1];
+            s[n-i-1]=temp;
         }
     }
 }
