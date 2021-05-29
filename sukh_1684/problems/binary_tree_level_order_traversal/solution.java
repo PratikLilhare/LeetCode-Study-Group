7e44872964c650f16fb9532a4c060977502744db
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-29 04:25:49 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- sukh_1684/problems/binary_tree_level_order_traversal/solution.java
+++ sukh_1684/problems/binary_tree_level_order_traversal/solution.java
@@ -15,20 +15,19 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        if(root==null){
-            return new ArrayList<>();
-        }
-        List<List<Integer>> ret = new ArrayList<>();  //n
-        Queue<TreeNode> nodes = new LinkedList<>();   //maximum width
 
+        List<List<Integer>> ret = new ArrayList<>();
+        if(root == null){
+            return ret;
+        }
+        Queue<TreeNode> nodes = new LinkedList<>();
         nodes.add(root);
-        int count=0;
-        while(!nodes.isEmpty()){        
+        while(!nodes.isEmpty()){
+            List<Integer> temp = new ArrayList<>();
             int size = nodes.size();
-             ret.add(new ArrayList<>());
             for(int i=0;i<size;i++){
-               TreeNode node = nodes.remove();
-                ret.get(ret.size()-1).add(node.val);
+                TreeNode node = nodes.remove();
+                temp.add(node.val);
                 if(node.left!=null){
                     nodes.add(node.left);
                 }
@@ -36,10 +35,8 @@ class Solution {
                     nodes.add(node.right);
                 }
             }
-            ;
-     
-            
+            ret.add(temp);
         }
-        return ret;
-    }          //time O(N)- total number of elements
+       return ret; 
+    }
 }
