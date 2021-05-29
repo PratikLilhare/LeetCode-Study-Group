4bdfb4f57d6f7e3cb6765c76dbdf7de179480eab
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-29 00:49:58 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (java)

--- sukh_1684/problems/intersection_of_two_arrays_ii/solution.java
+++ sukh_1684/problems/intersection_of_two_arrays_ii/solution.java
@@ -1,34 +1,37 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> a = new HashMap<Integer,Integer>();
-        if(nums1.length>nums2.length){
-            int [] ret= new int[nums2.length];
-            for(int i:nums1){
-                a.put(i,a.getOrDefault(i,0)+1);
+        HashMap<Integer, Integer> oneMap = new HashMap<>();
+        HashMap<Integer, Integer> twoMap = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            oneMap.merge(nums1[i], 1, Integer::sum);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            twoMap.merge(nums2[i], 1, Integer::sum);
+        }
+        for (Map.Entry<Integer, Integer> entry : oneMap.entrySet()) {
+            int key = entry.getKey(), value = entry.getValue();
+            if (twoMap.get(key) != null) {
+                int twoVal = twoMap.get(key);
+                oneMap.put(key, Math.min(value, twoVal));
+            } else {
+                oneMap.put(key, 0);
             }
-            int size=0;
-            for(int i:nums2){
-                if(a.containsKey(i)&&a.get(i)>0){
-                    ret[size++]=i;
-                    a.put(i,a.get(i)-1);
+        }
+        int size = 0;
+        for (int val : oneMap.values()) {
+            size += val;
+        }
+        int[] returnVal = new int[size];
+        int j = 0;
+        for (int item : oneMap.keySet()) {
+            if (oneMap.get(item) > 0) {
+                for (int i = 0; i < oneMap.get(item); i++) {
+                    returnVal[j] = item;
+                    j++;
                 }
             }
-            return(Arrays.copyOf(ret,size));
         }
-        else{
-             int [] ret= new int[nums1.length];
-            for(int i:nums2){
-                a.put(i,a.getOrDefault(i,0)+1);
-            }
-            int size=0;
-            for(int i:nums1){
-                if(a.containsKey(i)&&a.get(i)>0){
-                    ret[size++]=i;
-                    a.put(i,a.get(i)-1);
-                }
-            }
-            return(Arrays.copyOf(ret,size));
-            
-        }
-}
+        return returnVal;
+        
+    }
 }
