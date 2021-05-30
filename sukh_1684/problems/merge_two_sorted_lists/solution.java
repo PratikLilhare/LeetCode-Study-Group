6f54767e51c8364087606439cb6012c4242470fe
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-30 04:17:55 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- sukh_1684/problems/merge_two_sorted_lists/solution.java
+++ sukh_1684/problems/merge_two_sorted_lists/solution.java
@@ -10,6 +10,76 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode current = null;
+        if(l1==null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+         if(l1.val <= l2.val){
+                head = l1;
+                current = head;
+                l1= l1.next;
+            }
+            else{
+                head = l2;
+                current = head;
+                l2= l2.next;
+                
+            }
+        while(l1!=null && l2!=null){
+            if(l1.val <= l2.val){
+                current.next = l1;
+                l1= l1.next;
+            }
+            else{
+                current.next = l2;
+                l2= l2.next;
+            }
+            current = current.next;
+        }
+        while(l1!=null){
+            current.next = l1;
+            l1= l1.next;
+            current = current.next;
+        }
+          while(l2!=null){
+            current.next = l2;
+            l2= l2.next;
+            current = current.next;
+        }
+        return head;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
         if(l1==null && l2==null){
             return null;
         }
@@ -53,5 +123,5 @@ class Solution {
                  l2=l2.next;
         }
         return head;
-    }
-}
+        
+        */
