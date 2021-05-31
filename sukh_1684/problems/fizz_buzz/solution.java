b862e3c1fe1860957cbd836c1c8cd45f6fa9021b
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2021-05-31 16:29:57 +0000

    Sync LeetCode submission - Fizz Buzz (java)

--- sukh_1684/problems/fizz_buzz/solution.java
+++ sukh_1684/problems/fizz_buzz/solution.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList<String>();
+        List<String> ret = new ArrayList<>();
         for(int i=1;i<=n;i++){
-            if(i%3==0&&i%5==0){
-                result.add("FizzBuzz");
+            if(i%3 == 0 && i%5 == 0){
+                ret.add("FizzBuzz");
             }
-            else if(i%3==0){
-                result.add("Fizz");
+            else if(i%3 == 0){
+                ret.add("Fizz");
             }
-            else if(i%5==0){
-                result.add("Buzz");
+            else if(i%5 == 0){
+                ret.add("Buzz");
             }
             else{
-                result.add(i+"");
+                ret.add(""+i);
             }
         }
-        return result;
+        return ret;
     }
 }
