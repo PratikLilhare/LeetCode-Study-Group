f90d8f6630ae6a12e4f39a58b3146386249d7a8f
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2022-07-19 05:45:14 +0000

    Sync LeetCode submission - Palindrome Number (javascript)

--- sukh_1684/problems/palindrome_number/solution.js
+++ sukh_1684/problems/palindrome_number/solution.js
@@ -3,16 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = (x) => {
-    if(x<0) return false;
-    var temp = x;
-    var y=0;
-    while(temp!=0){
-        y = y*10+temp%10;
-        temp = Math.floor(temp/10);    
+    if(x<0){
+        return false;
     }
-    if(x === y){
-        return true;
+    let temp = x;
+    let y = 0;
+    while(temp>0){
+        y=y*10+temp%10;
+        temp = Math.floor(temp/10);
     }
-    return false;
+    return x === y;
     
 };
