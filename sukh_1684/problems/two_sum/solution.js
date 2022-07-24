bd97348b0dc8e725f0329b3ff17802a9d99c0d6e
Sukhwinder Singh <62976859+sukhsfu@users.noreply.github.com>
2022-07-24 07:09:39 +0000

    Sync LeetCode submission - Two Sum (javascript)

--- sukh_1684/problems/two_sum/solution.js
+++ sukh_1684/problems/two_sum/solution.js
@@ -3,15 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = (nums, target)  => {
-    const map = new Map();
-    for(i=0;i<nums.length;i++){
-        map.set(nums[i],i);
+var twoSum = function(nums, target) {
+    let map = new Map();
+    let ans = [];
+    for(let i=0; i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            ans.push(i,map.get(target-nums[i]));
+            break;
+        }
+        else{
+            map.set(nums[i],i);
+        }
     }
-    for(i=0;i<nums.length-1;i++){
-      if(map.has(target-nums[i]) && map.get(target-nums[i])!=i){
-          return [i,map.get(target-nums[i])];
-      }
-    }
-     
+    return ans;
 };
